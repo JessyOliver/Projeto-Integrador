050c653ef057d2d1e93ca6f46ed44a762e6d79ca
Juan Barros <59633047+juanhdev@users.noreply.github.com>
2021-08-04 11:54:42 -0300

    Update Postagem.java

--- SpringDoProjeto/mandala/src/main/java/br/org/generation/mandala/model/Postagem.java
+++ SpringDoProjeto/mandala/src/main/java/br/org/generation/mandala/model/Postagem.java
@@ -9,6 +9,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 
+//Postagem model
 @Entity
 @Table (name = "tb_postagens")
 public class Postagem {
