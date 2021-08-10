efaddda4fa5cdde83e7d436ebf8fa10db8497b50
JessyOliver <jessy22oliver@gmail.com>
2021-08-10 08:23:46 -0300

    Atualizando o campo data do codigo

--- SpringDoProjeto/mandala/src/main/java/br/org/generation/mandala/model/Usuario.java
+++ SpringDoProjeto/mandala/src/main/java/br/org/generation/mandala/model/Usuario.java
@@ -2,6 +2,7 @@ package br.org.generation.mandala.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,6 +10,8 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 @Table (name = "tb_usuario")
@@ -23,6 +26,8 @@ public class Usuario {
 	private String nome;
 	
 	@NotNull (message = "O atributo data de nascimento precisa ser preenchido")
+	@Column(name = "dt_nascimento")
+	@JsonFormat(pattern = "yyyy-MM-dd")
 	private LocalDate dtNascimento; 
 
 	@NotNull (message = "O atributo gênero precisa ser preenchido")
@@ -96,19 +101,6 @@ public class Usuario {
 		this.senha = senha;
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		
 
 }
